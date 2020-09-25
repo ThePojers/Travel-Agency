1028421b8e32825b82321975b6998d9e52ec25d3
Dawid Perzyński <loginosobisty@o2.pl>
2020-09-25 18:30:39 +0200

    Add formatted time

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './HappyHourAd.scss';
+import { formatTime } from '../../../utils/formatTime';
 
 class HappyHourAd extends React.Component {
 
@@ -37,7 +38,7 @@ class HappyHourAd extends React.Component {
       return(
         <div className={styles.component}>
           <h3 className={styles.title}>{title}</h3>
-          <div className={styles.promoDescription}>{happyHourTime > 23*60*60 ? promoDescription : happyHourTime}</div>
+          <div className={styles.promoDescription}>{happyHourTime > 23*60*60 ? promoDescription : formatTime(happyHourTime)}</div>
         </div>
       );
     }    
--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,2 +1,24 @@
 
-export const formatTime = () => 'formatted time';
+
+export const formatTime = (arg) => {
+  console.log(isNaN(arg));
+  if(arg ===  undefined){
+    return null;
+  } else if (isNaN(arg)){
+    return null;
+  } else if (arg < 0){
+    return null;
+  } else {
+    const seconds = Math.floor(arg%60).toString().padStart(2, '0');
+
+    const minQuotient = arg/60;
+    const minutes = Math.floor(minQuotient%60).toString().padStart(2, '0');
+
+    const hourQuotient = arg/3600;
+    const hours = Math.floor(hourQuotient%60).toString().padStart(2, '0');
+
+    return hours + ':' + minutes + ':' + seconds;
+  }
+
+
+};
